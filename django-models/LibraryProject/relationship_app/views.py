756a824e81d0e506e8fe8b6bf91be3a19d6d02de
“Joseph <akpannath84@gmail.com>
2025-02-22 23:58:06 -0800

    Added views, URL configurations, and templates for relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,18 +7,15 @@ from .models import Book
 from django.views.generic import DetailView
 from .models import Library
 
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "library_detail.html"
-    context_object_name = "library"
 
 def list_books(request):
     books = Book.objects.all()
-    book_list = "<h1>Books Available:</h1><ul>"
-    for book in books:
-        book_list += f"<li>{book.title} by {book.author.name}</li>"
-    book_list += "</ul>"
-    return HttpResponse(book_list)
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
 
