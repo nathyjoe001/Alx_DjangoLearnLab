b735d0ca1ee2d559e6dc4443e402a9d07d72dc9c
“Joseph <akpannath84@gmail.com>
2025-02-23 00:16:05 -0800

    Fixed missing import for Library model in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,15 @@
-
-# Create your views here.
 from django.shortcuts import render
-from django.views.generic.detail import DetailView  # Correct import
-from .models import Book, Library
-from django.http import HttpResponse
+from django.views.generic.detail import DetailView  # Import DetailView
+from .models import Book, Library  # Ensure Library is imported
 
 # Function-based view to list all books
-
-
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+# Class-based view to display details of a library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
 
