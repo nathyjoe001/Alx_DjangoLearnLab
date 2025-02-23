293f6d966c3632f53a444ababd49a1d7206bd096
“Joseph <akpannath84@gmail.com>
2025-02-23 00:11:47 -0800

    Fixed import for DetailView in LibraryDetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,16 @@
 
 # Create your views here.
-
 from django.shortcuts import render
+from django.views.generic.detail import DetailView  # Correct import
+from .models import Book, Library
 from django.http import HttpResponse
-from .models import Book
+
+# Function-based view to list all books
 
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
-
-from django.views.generic import DetailView
-from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
