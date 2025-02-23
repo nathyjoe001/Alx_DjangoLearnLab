75c48ee89f8adfd2569787268cf1461fb3cbc6a2
“Joseph <akpannath84@gmail.com>
2025-02-23 01:34:16 -0800

    Implementing RegisterView templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,17 @@
+
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
+from .views import RegisterView  # Import the RegisterView from views.py
 from .views import list_books, LibraryDetailView, register
 
+
+
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
-    path("register/", register, name="register"),
+
 ]
