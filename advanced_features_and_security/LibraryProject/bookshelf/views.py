53a3d0181f2b3ce8f3e01e0fba530596770b8a9e
“Joseph <akpannath84@gmail.com>
2025-03-02 15:03:06 -0800

    Added book_list view with permission checks

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
+@permission_required('bookshelf.view_book', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 # Create your views here.
