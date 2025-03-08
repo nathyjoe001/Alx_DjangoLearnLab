da1b963fd68b780afd9158484315eb67ebf696a3
NathyJoe001 <akpannath84@gmail.com>
2025-03-07 22:38:46 -0800

    Adding API Endpoint with Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()  # Query to fetch all books from the database
+    serializer_class = BookSerializer  # Use the BookSerializer to convert model data to JSON
