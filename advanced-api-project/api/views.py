9019069b5c13276d8f7678ee3a6b976018e9a371
NathyJoe001 <akpannath84@gmail.com>
2025-03-14 16:17:36 +0100

    adding delete and update endpoints

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 # api/views.py
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
