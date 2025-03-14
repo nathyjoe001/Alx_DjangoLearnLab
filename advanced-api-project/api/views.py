cbe81ffdf5714ec7fe412dea2b01dcd6c2adc332
NathyJoe001 <akpannath84@gmail.com>
2025-03-14 16:29:12 +0100

    Added DFR permission to API views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 # api/views.py
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
