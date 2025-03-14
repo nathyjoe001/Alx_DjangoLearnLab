7c251036d83f7e621918a12fd59f11c575066fa2
NathyJoe001 <akpannath84@gmail.com>
2025-03-14 16:25:01 +0100

    Added correct update and delete endpoints

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,13 +1,13 @@
 # api/urls.py
 from django.urls import path
-from api import views
+from api.views import BookListView, BookCreateView, BookDetailView,BookUpdateView,BookDeleteView
 
 urlpatterns = [
-    path('books/', views.BookListView.as_view(), name='book-list'),  # List and create books
-    path('books/create/', views.BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # Retrieve a single book by ID
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/', BookListView.as_view(), name='book-list'),  # List and create books
+    path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
+    path('books/<int:pk>/',BookDetailView.as_view(), name='book-detail'),  # Retrieve a single book by ID
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),  # Update a book
+    path('books/delete/<int:pk>/',BookDeleteView.as_view(), name='book-delete'),  # Delete a book
 ]
 
 
