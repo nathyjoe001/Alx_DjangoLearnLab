5ca0037ba29bfcd3975c864a07cefd75476d5674
NathyJoe001 <akpannath84@gmail.com>
2025-03-30 22:27:41 +0100

    Adding follow and unfollow comments

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,45 +1,41 @@
-from rest_framework import status, views, permissions
+from django.shortcuts import render
+from rest_framework import status, generics, permissions
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from django.contrib.auth import get_user_model, authenticate
-from rest_framework.exceptions import NotFound
 from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import NotFound
+from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
 
 User = get_user_model()
 
 # Register User
-class RegisterView(views.APIView):
+class RegisterView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+
     def post(self, request):
-        # Initialize the User serializer with the data sent in the request
-        serializer = UserSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         
-        # Validate the data and create the user
         if serializer.is_valid():
             user = serializer.save()
-            # Create the authentication token for the new user
             token = Token.objects.create(user=user)
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         
-        # If the serializer is invalid, return the errors
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Login User
-class LoginView(views.APIView):
+class LoginView(generics.GenericAPIView):
     def post(self, request):
-        # Get the username and password from the request
         username = request.data.get('username')
         password = request.data.get('password')
 
-        # Authenticate the user
         user = authenticate(username=username, password=password)
 
         if user:
-            # If user is authenticated, create or retrieve a token
             token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key})
         
-        # If the credentials are invalid, return an error message
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
 # Follow User
