4a7e6264c07286ec7fbc8fa5b9f519b2d2e5c319
NathyJoe001 <akpannath84@gmail.com>
2025-03-30 22:58:19 +0100

    Adding like and unlike post

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -3,6 +3,13 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from .models import Post, Like
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+
+
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -23,3 +30,34 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+    
+   
+
+User = get_user_model()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f'{self.user.username} liked {self.post.title}'
+
+
+User = get_user_model()
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+    actor = models.ForeignKey(User, related_name='actor_notifications', on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)  # e.g., 'liked your post'
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    target_object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('target_content_type', 'target_object_id')
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.actor.username} {self.verb} {self.target}'
